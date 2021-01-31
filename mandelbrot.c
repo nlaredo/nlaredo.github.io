a6414f042b2be26b0c9c70e404ae3f22560baeac
Nathan Laredo <github@nathanlaredo.com>
2021-01-31 06:33:25 -0800

    work in progress arbitrary precision floats for mandelbrot

--- mandelbrot.c
+++ mandelbrot.c
@@ -12,11 +12,12 @@ EMSCRIPTEN_KEEPALIVE int loglen = 0;
 EMSCRIPTEN_KEEPALIVE int pixelMin = 128 * 128;
 EMSCRIPTEN_KEEPALIVE int width, height, pixelCount, pixelMax = 0;
 EMSCRIPTEN_KEEPALIVE int cx, cy, xpos, ypos, cdx, cdy;
-EMSCRIPTEN_KEEPALIVE double dscale, xmin, ymin, maxIter;
+EMSCRIPTEN_KEEPALIVE double xfocus, yfocus, dscale, maxIter, jx, jy;
 EMSCRIPTEN_KEEPALIVE int pixel = 0;
 EMSCRIPTEN_KEEPALIVE int smooth = 1;
 
-void EMSCRIPTEN_KEEPALIVE jprintf(char *fmt, ...) {
+// jsprintf will be forwarded to javascript debug console
+void EMSCRIPTEN_KEEPALIVE jsprintf(char *fmt, ...) {
   va_list args;
   va_start(args, fmt);
   loglen += vsprintf(logstart + loglen, fmt, args);
@@ -30,7 +31,8 @@ int* EMSCRIPTEN_KEEPALIVE init(int pixels) {
 }
 
 void EMSCRIPTEN_KEEPALIVE setupSize(int w, int h, int i, int s,
-                                    double ds, double xm, double ym) {
+                                    double xf, double yf,
+                                    double ds, double x0, double y0) {
   if (w < 8) w = 8;
   if (h < 8) h = 8;
   if (i < 8) i = 8;
@@ -41,26 +43,28 @@ void EMSCRIPTEN_KEEPALIVE setupSize(int w, int h, int i, int s,
   height = h;
   maxIter = i;
   smooth = s;
+  xfocus = xf;
+  yfocus = yf;
   cx = w >> 1;
   cy = h >> 1;
   pixel = xpos = ypos = cdx = 0; cdy = -1;
   dscale = ds;
-  xmin = xm;
-  ymin = ym;
+  jx = x0;
+  jy = y0;
 }
 
-double EMSCRIPTEN_KEEPALIVE mandelbrot(int px, int py) {
-  double x0 = px * dscale + xmin;
-  double y0 = py * dscale + ymin;
-  double x = x0; //z.r
-  double y = y0; //z.i
-  double i = 0;
+double EMSCRIPTEN_KEEPALIVE mandelbrot(int px, int py, double x0, double y0) {
+  double x = (px - cx) * dscale + xfocus; // z.r
+  double y = (py - cy) * dscale + yfocus; // z.i
   double xsq = x*x;
   double ysq = y*y;
+  double i = 0;
+  if (!x0 && !y0) {  // if these are non-zero, julia; else mandelbrot
+    x0 = x;
+    y0 = y;
+  }
   do {
-    y = x * y;
-    y += y;
-    y += y0;
+    y = 2 * x * y + y0;
     x = xsq - ysq + x0;
     xsq = x * x;
     ysq = y * y;
@@ -77,9 +81,9 @@ int EMSCRIPTEN_KEEPALIVE calcWork(int workUnit) {
     // calculate in spiral from center
     int x = cx + xpos;
     int y = cy + ypos;
-    if (x >= 0 && x < width && y >= 0 && y <= height) {
+    if (x >= 0 && x < width && y >= 0 && y < height) {
       int o = y * width + x;
-      double it = mandelbrot(x, y);
+      double it = mandelbrot(x, y, jx, jy);
       int i = floor(it);
       if (smooth && i < maxIter) {
         double e = (float)(i + 1) - it;
